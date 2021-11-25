f47940c9057289368c82ba9df751b272e4c397a9
Ysteimnmetz <62920334+YSteinmetz@users.noreply.github.com>
2021-11-24 23:43:59 -0300

    troublesInUpdating

--- src/components/StatusBox/index.tsx
+++ src/components/StatusBox/index.tsx
@@ -12,39 +12,81 @@ type Risks = {
 
 export function StatusBox(){
     var counter = 1;
-    var test = false;
+    var test = "1";
+
+    var alerta = {
+        Luminosidade: "0",
+        Temperatura: "0",
+        Vibracao: "0",
+        Ruido: "0"
+    }
+    var risco = {
+        Luminosidade: "0",
+        Temperatura: "0",
+        Vibracao: "0",
+        Ruido: "0"
+    }
+    var dispImg = {
+        Luminosidade: imagesWrapper.luminosidadeImages.normalLuminosidade,
+        Temperatura: imagesWrapper.temperaturaImages.normaltemperatura,
+        Vibracao: imagesWrapper.vibracaoImages.normalVibracao,
+        Ruido: imagesWrapper.ruidoImages.normalRuido
+    }
+    
+
+    
     const [calling, setCalling] = useState(false);
 
     useEffect(()=>{
         setInterval(()=>{
-            console.log("I'm here", counter);
+            if(counter >= 10){
+                counter = 0;
+                //console.log("I'm here");
+                //getUserState();
+            }            
             counter++;
-            if(counter == 2){
-                setCalling(true);
-            }
-            if(counter >= 3){
-                setCalling(false);
-                counter = 1;
-            }
-        },5000);
-    },[3]);
+        },1000);
+    },[]);
 
     
    
     async function getUserState(){
-        await api.get('alarmsWarning').then(response => {
-            console.log(response.data);
-        })
+        await api.get<Risks>('alarmsWarning').then(response => {
+            alerta.Luminosidade = response.data.Luminosidade;
+            alerta.Temperatura = response.data.Temperatura;
+            alerta.Vibracao = response.data.Vibracao;
+            alerta.Ruido = response.data.Ruido;
+     
+        });
+        await api.get<Risks>('alarmsRisk').then(response => {
+            risco.Luminosidade = response.data.Luminosidade;
+            risco.Temperatura = response.data.Temperatura;
+            risco.Vibracao = response.data.Vibracao;
+            risco.Ruido = response.data.Ruido;
+            
+        });
+
+        if(risco.Luminosidade == "1"){
+            
+            dispImg.Luminosidade == imagesWrapper.luminosidadeImages.riscoLuminosidade;
+        }else if(alerta.Luminosidade == "1"){
+            
+            dispImg.Luminosidade == imagesWrapper.luminosidadeImages.alertaLuminosidade;
+        }else{
+            
+            dispImg.Luminosidade == imagesWrapper.luminosidadeImages.normalLuminosidade;
+        }
+
+        console.log(dispImg.Luminosidade);
     }
 
     return(
         <div className={styles.contentWrapper}>
-            <p>{!!calling ? "Liberar operador" : "Chamar operador"}</p>
             <div className={styles.contentIcons}>
-                <img src={imagesWrapper.vibracaoImages.normalVibracao} alt="Vibracao" />
-                <img className={styles.contentIconLuminosidade} src={ !!calling ? !!test ? imagesWrapper.luminosidadeImages.alertaLuminosidade : imagesWrapper.luminosidadeImages.riscoLuminosidade : imagesWrapper.luminosidadeImages.normalLuminosidade} alt="Luminosidade" />
-                <img className={styles.contentIconRuido} src={imagesWrapper.ruidoImages.riscoRuido} alt="Ruido" />
-                <img className={styles.contentIconTemperatura} src={imagesWrapper.temperaturaImages.normaltemperatura} alt="Temperatura" />
+                <img src={dispImg.Vibracao} alt="Vibracao" />
+                <img className={styles.contentIconLuminosidade} src={ dispImg.Luminosidade } alt="Luminosidade" />
+                <img className={styles.contentIconRuido} src={dispImg.Ruido} alt="Ruido" />
+                <img className={styles.contentIconTemperatura} src={dispImg.Temperatura} alt="Temperatura" />
             </div>
         </div>
     )
